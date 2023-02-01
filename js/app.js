61dcfd6e2d16dc3255b958f8087b9563071986c3
Thiago Levi <thiago.levi@outlook.com>
2023-02-01 13:45:35 -0300

    Add functions js to Errors

--- js/app.js
+++ js/app.js
@@ -11,17 +11,26 @@ const listOfRepositories = document.querySelector(
   '[data-js="listOfRepositories"]'
 );
 
+const showError = () => {
+  userImgHtml.src = "./src/img/icons8-github.svg";
+  userNameHtml.innerHTML = "Ops... Dev Não encontratdo";
+  bioHtml.innerHTML = "Ops... Dev Não encontratdo";
+  listOfRepositories.innerHTML = `<h1>Dá uma olhadinha melhor no nome do usuário que você digitou :) </h1>`;
+};
+
 const requestTo = async (endPoint) => {
   try {
     const data = await fetch(endPoint);
 
     if (!data.ok) {
       console.log("Status not Ok");
+      showError();
     }
+
     return await data.json();
   } catch (error) {
-    alert(`ATENÇÂO: ${error.message}`);
     console.log(error);
+    showError();
   }
 };
 
@@ -30,8 +39,8 @@ const togglesContainers = () => {
   newRootContainer.classList.toggle("display-none");
 };
 
-const getUserInfos = async (inputValue) => {
-  const endPointToUserInfos = `https://api.github.com/users/${inputValue}`;
+const getUserInfos = async (userNameInputted) => {
+  const endPointToUserInfos = `https://api.github.com/users/${userNameInputted}`;
   const userInfosParsed = await requestTo(endPointToUserInfos);
   return userInfosParsed;
 };
@@ -41,36 +50,45 @@ const getRepositories = async (url) => {
   return repositoriesParsed;
 };
 
-const renderRepositories = (repositories) => {
-  const string = repositories.reduce(
-    (acc, { name, description, html_url, language }) => {
-      return (acc += `<li class="repository">
-    <p class="repository-name"><span>Nome&#58;&ensp;</span>${name}</p>
-    <p class="repository-description"><span>Descrição&#58;&ensp;</span>${description}</p>
-    <p class="main-laguage"><span>Linguagem principal&#58;&ensp;</span>${language}</p>
-    <a href="${html_url}" target="_blank" rel="noopener noreferrer"><button class="button-to-repository">Repositório</button></a>
-    </li>`);
-    },
-    ""
-  );
-
-  return string;
-};
-
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-
-  const inputValue = event.target.inputSearch.value;
-  console.log(inputValue);
-  togglesContainers();
-
-  const userInfosParsed = await getUserInfos(inputValue);
+const renderUserInfosOnCard = (userInfosParsed) => {
   const { avatar_url, name, bio, repos_url } = userInfosParsed;
 
   userImgHtml.src = avatar_url;
   userNameHtml.innerHTML = name;
   bioHtml.innerHTML = bio;
+};
 
+const renderRepositories = (repositories) => {
+  const htmlTemplateOfRepositories = repositories.reduce(
+    (acc, { name, description, html_url, language }) => {
+      return (acc += `<li class="repository">
+    <p class="repository-name"><span>Nome&#58;&ensp;</span>${name ?? "-"}</p>
+    <p class="repository-description"><span>Descrição&#58;&ensp;</span>${
+      description ?? "-"
+    }</p>
+    <p class="main-laguage"><span>Linguagem principal&#58;&ensp;</span>${
+      language ?? "-"
+    }</p>
+    <a href="${
+      html_url ?? "-"
+    }" target="_blank" rel="noopener noreferrer"><button class="button-to-repository">Repositório</button></a>
+    </li>`);
+    },
+    ""
+  );
+
+  return htmlTemplateOfRepositories;
+};
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const userNameInputted = event.target.inputSearch.value;
+  const userInfosParsed = await getUserInfos(userNameInputted);
+  renderUserInfosOnCard(userInfosParsed);
+  togglesContainers();
+
+  const { repos_url } = userInfosParsed;
   const repositories = await getRepositories(repos_url);
   listOfRepositories.innerHTML = renderRepositories(repositories);
 
